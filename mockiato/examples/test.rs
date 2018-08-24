c8ffb39bad557982d7d86c71e269c44cd65d4fef
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2018-08-24 19:05:39 +0200

    Import required items

--- mockiato/examples/test.rs
+++ mockiato/examples/test.rs
@@ -1,5 +1,9 @@
+#![feature(custom_attribute, plugin)]
+#![plugin(mockiato_codegen)]
+
+use std::fmt::Debug;
+
 #[mockable]
 trait Foo: Debug {}
 
-fn main() {
-}
+fn main() {}
--- mockiato_codegen/src/lib.rs
+++ mockiato_codegen/src/lib.rs
@@ -4,30 +4,33 @@
 
 extern crate rustc_plugin;
 extern crate syntax;
+extern crate syntax_pos;
 
 use rustc_plugin::Registry;
-use syntax::ext::base::{MultiItemDecorator, SyntaxExtension};
+use syntax::ast;
+use syntax::ext::base::{Annotatable, ExtCtxt, MultiItemDecorator, SyntaxExtension};
 use syntax::symbol::Symbol;
+use syntax_pos::Span;
 
-struct Mockable {}
+struct Mockable;
 
 impl MultiItemDecorator for Mockable {
     fn expand(
         &self,
-        ecx: &mut ExtCtxt,
+        cx: &mut ExtCtxt,
         sp: Span,
         meta_item: &ast::MetaItem,
         item: &Annotatable,
         push: &mut dyn FnMut(Annotatable),
     ) {
-        unimplemented!()
+        cx.parse_sess.span_diagnostic.span_note_without_error(sp, "Let's brew you some macchiato");
     }
 }
 
 #[plugin_registrar]
 pub fn plugin_registrar(reg: &mut Registry) {
-    registry.register_syntax_extension(
+    reg.register_syntax_extension(
         Symbol::intern("mockable"),
-        SyntaxExtension::MultiDecorator(),
+        SyntaxExtension::MultiDecorator(Box::new(Mockable)),
     );
 }
