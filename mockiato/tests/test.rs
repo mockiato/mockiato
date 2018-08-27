ca536ad65ed808442aea4aef28e92298d5f98e7c
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2018-08-27 14:20:04 +0200

    Parse attribute, generate derives

---
+++ mockiato/tests/test.rs
@@ -0,0 +1,29 @@
+#![feature(custom_attribute, plugin)]
+#![plugin(mockiato_codegen)]
+
+use std::fmt::{Debug, Display};
+
+#[mockable(derive(Debug))]
+trait Greeter<D>: Debug
+where
+    D: Display,
+{
+    fn say_hello(&self, name: D) -> String;
+}
+
+// #[derive(Debug)]
+// #[mockable]
+// struct GreeterMock {}
+
+//impl<D> Greeter<D> for GreeterMock
+//where
+//    D: Display,
+//{
+//    fn say_hello(&self, _name: D) -> String {
+//        unimplemented!();
+//    }
+//}
+#[test]
+fn test() {
+    let _mock = GreeterMock;
+}
--- mockiato_codegen/src/lib.rs
+++ mockiato_codegen/src/lib.rs
@@ -1,16 +1,28 @@
 #![crate_type = "dylib"]
-#![feature(quote, concat_idents, plugin_registrar, rustc_private)]
+#![feature(
+    quote,
+    concat_idents,
+    plugin_registrar,
+    rustc_private,
+    decl_macro
+)]
 #![feature(custom_attribute)]
 
 extern crate rustc_plugin;
+extern crate rustc_resolve;
 extern crate syntax;
 extern crate syntax_pos;
 
 use rustc_plugin::Registry;
 use syntax::ast::{
-    self, GenericBounds, Generics, Ident, IsAuto, Item, ItemKind, TraitItem, Unsafety,
+    self, Attribute, GenericBounds, Generics, Ident, IsAuto, ItemKind, LitKind, MetaItem,
+    MetaItemKind, NestedMetaItem, NestedMetaItemKind, Path, TraitItem, Unsafety, VariantData,
+    DUMMY_NODE_ID,
 };
 use syntax::ext::base::{Annotatable, ExtCtxt, MultiItemDecorator, SyntaxExtension};
+use syntax::ext::build::AstBuilder;
+use syntax::ptr::P;
+use syntax::source_map::Spanned;
 use syntax::symbol::Symbol;
 use syntax_pos::Span;
 
@@ -23,6 +35,24 @@ struct TraitDecl<'a> {
     generics: &'a Generics,
     generic_bounds: &'a GenericBounds,
     items: &'a [TraitItem],
+}
+
+#[derive(Debug)]
+struct MockableAttr {
+    derive_attr: Option<DeriveAttr>,
+    name_attr: Option<NameAttr>,
+}
+
+#[derive(Debug)]
+struct DeriveAttr {
+    span: Span,
+    list: Vec<NestedMetaItem>,
+}
+
+#[derive(Debug)]
+struct NameAttr {
+    symbol_span: Span,
+    symbol: Symbol,
 }
 
 impl<'a> TraitDecl<'a> {
@@ -55,13 +85,123 @@ impl<'a> TraitDecl<'a> {
     }
 }
 
+impl MockableAttr {
+    fn parse(cx: &mut ExtCtxt, meta_item: &ast::MetaItem) -> Option<Self> {
+        let mut derive_attr = None;
+        let mut name_attr = None;
+
+        match meta_item.node {
+            MetaItemKind::Word => {}
+            MetaItemKind::NameValue(..) => unreachable!(),
+            MetaItemKind::List(ref list) => {
+                let meta_items: Vec<_> = list
+                    .iter()
+                    .map(|nested| {
+                        match nested.node {
+                            NestedMetaItemKind::MetaItem(ref meta_item) => Some(meta_item),
+                            NestedMetaItemKind::Literal(_) => {
+                                // TODO: make more helpful
+                                cx.parse_sess
+                                    .span_diagnostic
+                                    .mut_span_err(nested.span(), "Unsupported syntax for #[mocked]")
+                                    .help("Use something like #[mocked(name = \"FooMock\", derive(Debug))]")
+                                    .emit();
+                                None
+                            }
+                        }
+                    }).collect();
+
+                for item in meta_items {
+                    match item {
+                        Some(item) => if item.ident == "derive" {
+                            if derive_attr.is_some() {
+                                cx.span_warn(item.span(), "`derive` is specified more than once. The latter definition will overwrite the former.");
+                            }
+                            derive_attr = DeriveAttr::parse(cx, item.clone());
+                        } else if item.ident == "name" {
+                            if name_attr.is_some() {
+                                cx.span_warn(item.span(), "`name` is specified more than once. The latter definition will overwrite the former.");
+                            }
+                            name_attr = NameAttr::parse(cx, item.clone());
+                        } else {
+                            cx.span_err(item.span(), "Syntax not supported");
+                            return None;
+                        },
+                        None => return None,
+                    }
+                }
+            }
+        }
+
+        Some(Self {
+            derive_attr,
+            name_attr,
+        })
+    }
+}
+
+impl DeriveAttr {
+    fn parse(cx: &mut ExtCtxt, meta_item: MetaItem) -> Option<Self> {
+        if let MetaItemKind::List(list) = meta_item.node {
+            Some(DeriveAttr {
+                span: meta_item.span,
+                list,
+            })
+        } else {
+            // TODO: make more helpful
+            cx.span_err(meta_item.span(), "Syntax not supported");
+            None
+        }
+    }
+
+    fn expand(self, cx: &mut ExtCtxt) -> Attribute {
+        cx.attribute(
+            self.span,
+            MetaItem {
+                ident: Path::from_ident(Ident::from_str("derive")),
+                node: MetaItemKind::List(self.list),
+                span: self.span,
+            },
+        )
+    }
+}
+
+impl NameAttr {
+    fn parse(cx: &mut ExtCtxt, meta_item: MetaItem) -> Option<Self> {
+        macro emit_error($cx: expr, $span:expr) {
+            $cx.parse_sess
+                .span_diagnostic
+                .mut_span_err($span, "`name` should be a string literal")
+                .help("Use something like #[mocked(name = \"FooMock\")]")
+                .emit();
+        };
+
+        match meta_item.node {
+            MetaItemKind::NameValue(Spanned { node, span }) => match node {
+                LitKind::Str(symbol, _) => Some(Self {
+                    symbol,
+                    symbol_span: span,
+                }),
+                _ => {
+                    emit_error!(cx, meta_item.span);
+                    None
+                }
+            },
+            _ => {
+                emit_error!(cx, meta_item.span);
+                None
+            }
+        }
+    }
+}
+
 struct Mockable;
 
 impl MultiItemDecorator for Mockable {
     fn expand(
         &self,
         cx: &mut ExtCtxt,
-        sp: Span,
+        _sp: Span,
         meta_item: &ast::MetaItem,
         item: &Annotatable,
         push: &mut dyn FnMut(Annotatable),
@@ -74,9 +214,33 @@ impl MultiItemDecorator for Mockable {
             }
         };
 
-        cx.parse_sess
-            .span_diagnostic
-            .span_note_without_error(item.span(), "Let's brew you some macchiato");
+        if trait_decl.unsafety == &Unsafety::Unsafe {
+            cx.span_err(item.span(), "#[mockable] does not support unsafe traits");
+            return;
+        }
+
+        let mockable_attr = match MockableAttr::parse(cx, meta_item) {
+            Some(mockable_attr) => mockable_attr,
+            None => return,
+        };
+
+        let mock_struct_ident = mockable_attr
+            .name_attr
+            .map(|attr| Ident::new(attr.symbol, attr.symbol_span))
+            .unwrap_or_else(|| Ident::from_str(&format!("{}Mock", trait_decl.ident)));
+
+        let mut mock_struct = cx
+            .item_struct(
+                meta_item.span,
+                mock_struct_ident,
+                VariantData::Unit(DUMMY_NODE_ID),
+            ).into_inner();
+
+        if let Some(derive_attr) = mockable_attr.derive_attr {
+            mock_struct.attrs.push(derive_attr.expand(cx));
+        }
+
+        push(Annotatable::Item(P(mock_struct)));
     }
 }
 
