e79f998dafaf6c85c5dcc7404de9082303452342
Ruben Schmidmeister <ruben@myelin.ch>
2018-11-05 19:09:30 +0100

    Remove unused Empty variant from Error

--- mockiato_codegen/src/result.rs
+++ mockiato_codegen/src/result.rs
@@ -4,33 +4,29 @@ pub(crate) type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Debug)]
 pub(crate) enum Error {
-    Empty,
     Diagnostic(Diagnostic),
     MultipleDiagnostics(Vec<Diagnostic>),
 }
 
 impl Error {
     /// Emits all [`Diagnostic`] messages stored in this error.
-    pub(crate) fn emit(self) -> Self {
-        self.emit_with(|d| d)
+    pub(crate) fn emit(self) {
+        self.emit_with(|d| d);
     }
 
     /// Emits all [`Diagnostic`] messages stored in this error.
     /// The passed [`Fn`] acts as a transformation function and is called for every
     /// [`Diagnostic`] in this error.
-    pub(crate) fn emit_with<F>(self, map_fn: F) -> Self
+    pub(crate) fn emit_with<F>(self, map_fn: F)
     where
         F: Fn(Diagnostic) -> Diagnostic,
     {
         match self {
-            Error::Empty => {}
             Error::Diagnostic(diagnostic) => map_fn(diagnostic).emit(),
             Error::MultipleDiagnostics(diagnostics) => {
                 diagnostics.into_iter().for_each(|d| map_fn(d).emit());
             }
         };
-
-        Error::Empty
     }
 
     /// Creates a new [`Error`] by merging an Iterator and collecting
@@ -42,7 +38,6 @@ impl Error {
         let mut collected = Vec::new();
 
         errors.for_each(|err| match err {
-            Error::Empty => {}
             Error::Diagnostic(diagnostic) => collected.push(diagnostic),
             Error::MultipleDiagnostics(mut diagnostics) => collected.append(&mut diagnostics),
         });
