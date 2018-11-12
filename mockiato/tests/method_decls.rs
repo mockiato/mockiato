53a2df1e7da53826f35a6ab35f446989715c26fc
Jeremy Stucki <jeremy@myelin.ch>
2018-11-12 14:18:35 +0100

    Replace lifetimes and generate bound lifetimes
    Co-authored-by: Ruben Schmidmeister <ruben@myelin.ch>

--- mockiato/tests/method_decls.rs
+++ mockiato/tests/method_decls.rs
@@ -9,6 +9,9 @@ trait Foo {
     fn arg_ref(&self, slice: &[u8]);
     fn arg_mut_ref(&self, buf: &mut [u8]);
     fn arg_ownership(&self, list: Vec<u32>);
+    fn explicit_lifetime<'a>(&self, buf: &'a mut [u8]);
+    fn explicit_lifetime_2<'a>(&self, names: &std::fmt::Formatter<'a>);
+    fn multiple_ref_params(&self, name: (&str, &str));
 }
 
 fn main() {}
--- mockiato_codegen/src/generate/argument_matcher.rs
+++ mockiato_codegen/src/generate/argument_matcher.rs
@@ -1,26 +1,27 @@
 use crate::parse::method_decl::MethodDecl;
 use crate::parse::method_inputs::MethodInputs;
 use heck::CamelCase;
-use proc_macro2::TokenStream;
-use syn::Ident;
+use proc_macro2::{Span, TokenStream};
+use syn::visit_mut::{visit_type_mut, visit_type_reference_mut, VisitMut};
+use syn::{BoundLifetimes, Ident, Lifetime, LifetimeDef, TypeReference};
 
 pub(crate) fn generate_argument_matcher(method_decl: &MethodDecl) -> TokenStream {
     let argument_matcher_ident = argument_matcher_ident(&method_decl.ident);
     let argument_matcher_fields = argument_matcher_fields(&method_decl.inputs);
 
     quote! {
-        pub(super) struct #argument_matcher_ident<'args> {
+        pub(super) struct #argument_matcher_ident {
             #argument_matcher_fields
         }
 
-        impl<'args> std::fmt::Debug for #argument_matcher_ident<'args> {
+        impl std::fmt::Debug for #argument_matcher_ident {
             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
                 // TODO
                 unimplemented!()
             }
         }
 
-        impl<'args> mockiato::internal::Arguments for #argument_matcher_ident<'args> {}
+        impl mockiato::internal::Arguments for #argument_matcher_ident {}
     }
 }
 
@@ -40,11 +41,57 @@ fn argument_matcher_fields(method_inputs: &MethodInputs) -> TokenStream {
         .iter()
         .map(|input| {
             let ident = &input.ident;
-            let ty = &input.ty;
+            let mut ty = input.ty.clone();
+
+            let mut lifetime_rewriter = LifetimeRewriter::default();
+            visit_type_mut(&mut lifetime_rewriter, &mut ty);
+
+            let bound_lifetimes = bound_lifetimes(lifetime_rewriter.lifetimes);
 
             TokenStream::from(quote!{
-                pub(super) #ident: #ty,
+                pub(super) #ident: std::boxed::Box<dyn #bound_lifetimes mockiato::internal::ArgumentMatcher<#ty>>,
             })
         })
         .collect()
+}
+
+fn bound_lifetimes(lifetimes: Vec<Lifetime>) -> Option<BoundLifetimes> {
+    if lifetimes.is_empty() {
+        return None;
+    }
+
+    Some(BoundLifetimes {
+        lifetimes: lifetimes
+            .into_iter()
+            .map(|lifetime| LifetimeDef::new(lifetime))
+            .collect(),
+        ..Default::default()
+    })
+}
+
+#[derive(Default)]
+struct LifetimeRewriter {
+    lifetimes: Vec<Lifetime>,
+}
+
+impl LifetimeRewriter {
+    fn create_new_lifetime(&mut self) -> Lifetime {
+        let lifetime = Lifetime::new(&format!("'arg{}", self.lifetimes.len()), Span::call_site());
+        self.lifetimes.push(lifetime.clone());
+        lifetime
+    }
+}
+
+impl VisitMut for LifetimeRewriter {
+    fn visit_lifetime_mut(&mut self, lifetime: &mut Lifetime) {
+        *lifetime = self.create_new_lifetime();
+    }
+
+    fn visit_type_reference_mut(&mut self, type_reference: &mut TypeReference) {
+        visit_type_reference_mut(self, type_reference);
+
+        if type_reference.lifetime.is_none() {
+            type_reference.lifetime = Some(self.create_new_lifetime());
+        }
+    }
 }
