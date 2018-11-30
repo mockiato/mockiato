1d97fc6c11a267dbcaa7afba9a9bbee046d9a945
Jeremy Stucki <jeremy@myelin.ch>
2018-11-30 15:03:25 +0100

    Remove Clone and Copy bounds
    Co-authored-by: Ruben Schmidmeister <ruben@myelin.ch>

--- mockiato/tests/auto_derive_trait_bounds.rs
+++ mockiato/tests/auto_derive_trait_bounds.rs
@@ -2,7 +2,7 @@ use mockiato_codegen::mockable;
 use std::fmt;
 
 #[mockable]
-trait Foo: fmt::Debug + Clone + Copy {}
+trait Foo: fmt::Debug {}
 
 fn main() {
     let _assert_debug: &fmt::Debug = &FooMock::new();
