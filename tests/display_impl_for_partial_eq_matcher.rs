d07c292c6452c46c9e34a6abf5ecf9e899639ca0
Ruben Schmidmeister <ruben@myelin.ch>
2019-04-22 13:43:01 +0200

    Correct test names

--- tests/display_impl_for_partial_eq_matcher.rs
+++ tests/display_impl_for_partial_eq_matcher.rs
@@ -14,7 +14,7 @@ trait Greeter {
 }
 
 #[test]
-fn partial_eq_matcher_has_debug_output() {
+fn partial_eq_matcher_has_display_output() {
     let matcher = partial_eq(Person {
         name: "Name".into(),
         age: 30,
@@ -31,7 +31,7 @@ fn partial_eq_matcher_has_debug_output() {
     expected = "The expected calls for GreeterMock::greet were not satisified.
 greet(Person { name: \"Name\", age: 30 }) -> \"Hello Name\" exactly 2 times, was called 0 times"
 )]
-fn partial_eq_matcher_has_debug_output_when_printed_as_expected_call() {
+fn partial_eq_matcher_has_display_output_when_printed_as_expected_call() {
     let mut greeter = GreeterMock::new();
     greeter
         .expect_greet(partial_eq(Person {
@@ -47,7 +47,7 @@ fn partial_eq_matcher_has_debug_output_when_printed_as_expected_call() {
     expected = "The expected calls for GreeterMock::greet_ref were not satisified.
 greet_ref(Person { name: \"Name\", age: 30 }) -> \"Hello Name\" exactly 2 times, was called 0 times"
 )]
-fn partial_eq_matcher_has_debug_output_for_reference_when_printed_as_expected_call_with() {
+fn partial_eq_matcher_has_display_output_for_reference_when_printed_as_expected_call_with() {
     let person = Person {
         name: "Name".into(),
         age: 30,
