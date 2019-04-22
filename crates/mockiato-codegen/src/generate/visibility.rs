2646ae22ee72d656275485df1b7464599b80cedb
Ruben Schmidmeister <ruben@myelin.ch>
2019-04-22 22:46:07 +0200

    Refactor raise_visibility_by_one_level

--- crates/mockiato-codegen/src/generate/visibility.rs
+++ crates/mockiato-codegen/src/generate/visibility.rs
@@ -1,33 +1,44 @@
-use syn::{parse_quote, Visibility};
+use std::iter;
+use syn::{parse_quote, PathSegment, VisRestricted, Visibility};
 
 pub(super) fn raise_visibility_by_one_level(visibility: &Visibility) -> Visibility {
     match visibility {
-        Visibility::Public(_) | Visibility::Crate(_) => visibility.clone(),
         Visibility::Inherited => parse_quote!(pub(super)),
-        Visibility::Restricted(inner_visibility)
-            if inner_visibility.path.segments[0].ident == "crate" =>
-        {
-            visibility.clone()
-        }
-        Visibility::Restricted(inner_visibility)
-            if inner_visibility.path.segments[0].ident == "self" =>
-        {
-            let mut inner_visibility = inner_visibility.clone();
-            inner_visibility.path.segments[0].ident = parse_quote!(super);
-            Visibility::Restricted(inner_visibility)
-        }
-        Visibility::Restricted(inner_visibility) => {
-            let mut inner_visibility = inner_visibility.clone();
-            inner_visibility
-                .path
-                .segments
-                .insert(0, parse_quote!(super));
-
-            if inner_visibility.in_token.is_none() {
-                inner_visibility.in_token = Some(parse_quote!(in));
-            }
-
-            Visibility::Restricted(inner_visibility)
+        Visibility::Public(_) | Visibility::Crate(_) => visibility.clone(),
+        Visibility::Restricted(restricted) if has_leading_colon(restricted) => visibility.clone(),
+        Visibility::Restricted(restricted) => {
+            Visibility::Restricted(raise_restricted_visibility_by_one_level(restricted))
         }
     }
+}
+
+fn has_leading_colon(visibility: &VisRestricted) -> bool {
+    visibility.path.leading_colon.is_some()
+}
+
+fn raise_restricted_visibility_by_one_level(visibility: &VisRestricted) -> VisRestricted {
+    let mut visibility = visibility.clone();
+
+    match &visibility.path.segments[0].ident {
+        ident if ident == "crate" => {}
+        ident if ident == "self" => replace_self_with_super_in_visibility(&mut visibility),
+        _ => prefix_visibility_with_super(&mut visibility),
+    }
+
+    visibility
+}
+
+fn replace_self_with_super_in_visibility(visibility: &mut VisRestricted) {
+    visibility.path.segments[0] = super_path_segment();
+}
+
+fn prefix_visibility_with_super(visibility: &mut VisRestricted) {
+    visibility.in_token = Some(parse_quote!(in));
+    visibility.path.segments = iter::once(super_path_segment())
+        .chain(visibility.path.segments.iter().cloned())
+        .collect();
+}
+
+fn super_path_segment() -> PathSegment {
+    parse_quote!(super)
 }
