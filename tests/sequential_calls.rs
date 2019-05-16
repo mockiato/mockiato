24f98184a28f266f6efc94100c8733e3e708b77b
Ruben Schmidmeister <ruben@myelin.ch>
2019-05-16 20:27:30 +0200

    Add regression test

--- tests/sequential_calls.rs
+++ tests/sequential_calls.rs
@@ -18,3 +18,18 @@ fn sequential_calls() {
     assert!(!mock.bar());
     assert!(mock.bar());
 }
+
+#[test]
+fn sequential_calls_work_with_expected_call_amount_with_no_upper_limit() {
+    let mut mock = FooMock::new();
+
+    mock.expect_bar_calls_in_order();
+    mock.expect_bar().returns(true).times(3..);
+    mock.expect_bar().returns(false).times(..);
+
+    for _ in 0..3 {
+        assert!(mock.bar());
+    }
+
+    assert!(!mock.bar());
+}
